406fa13f5a4a6fc3916ff07b713e80b464d0d35b
Shoaib-Saikat <58690026+Shoaib-Saikat@users.noreply.github.com>
2021-04-10 21:34:37 +0600

    Create Transcribing DNA into RNA (RNA).py

---
+++ Transcribing DNA into RNA (RNA).py
@@ -0,0 +1,14 @@
+#importing module to use functions
+import DNA_toolbox as DT
+
+#taking filename as input
+fn =('Enter filename:')
+
+#opening file and counting bases from imported function
+with open(fn,'r') as fh:
+    #reifining DNA bases
+    ref = DT.base_refine(fh.read())
+    #converting to RNA
+    RNAseq = DT.d_to_r(ref)
+    #printing RNA sequence
+    print(RNAseq)
